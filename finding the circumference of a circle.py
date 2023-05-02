59586a1dc00d84bff10c499c1faf04bf970969bf
ezeamii <131669112+ezeamii@users.noreply.github.com>
2023-05-02 13:06:13 +0100

    Add files via upload

---
+++ finding the circumference of a circle.py
@@ -0,0 +1,7 @@
+# PROGRAM TO FIND THE CIRCUMFERENCE OF A CIRCLE
+radius = float(input("Input the radius of the circle: "))
+pi = 3.14
+
+# we put in the formula of finding our circumference
+Circumference = (2 * pi * radius)
+print("The circumference of this circle is", Circumference, "cm")
